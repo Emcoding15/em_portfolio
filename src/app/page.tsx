7ebc822c012ec693c8a8fa6a763f08d22faa6cb6
EmCoding15 <jmguerra015@gmail.com>
2025-09-12 07:33:21 +0800

    refactor: remove Header and SectionTabs components for cleaner structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,15 @@
 "use client";
 // import Header from "./Header";
-import SectionTabs from "../components/SectionTabs";
 import AboutSection from "../sections/AboutSection";
 import HeroSection  from "../sections/HeroSection";
 import ProjectsSection from "../sections/ProjectsSection";
 import ContactSection from "../sections/ContactSection";
 import ChatbotWidget from "../components/ChatbotWidget";
 
+
 export default function Home() {  
   return (
     <main className="relative">
-      <SectionTabs />
       <div>
         <HeroSection />
         <AboutSection />
