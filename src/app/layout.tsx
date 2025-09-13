cb15cae54f10506cdd35462deac273aab0821dac
EmCoding15 <jmguerra015@gmail.com>
2025-09-13 13:54:29 +0800

    feat: add ScrollToTop component to enhance user navigation experience

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+import ScrollToTop from "../components/ScrollToTop";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <ScrollToTop />
         {children}
       </body>
     </html>
