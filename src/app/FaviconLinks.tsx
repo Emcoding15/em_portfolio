46208322c2dc9329d0fe2e15353656f167d1d632
EmCoding15 <jmguerra015@gmail.com>
2025-09-18 19:20:11 +0800

    Add FaviconLinks component to manage favicon links in the application

---
+++ src/app/FaviconLinks.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function FaviconLinks() {
+  return (
+    <>
+      <link rel="icon" type="image/png" href="/em_logo.png" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/em_logo.png" sizes="16x16" />
+      {/* Optionally add more sizes or apple-touch-icon here */}
+    </>
+  );
+}
